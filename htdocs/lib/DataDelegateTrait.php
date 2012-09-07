4e2e13d418eed6aed6ccbbc0b39aa2e3a724d039
milkliker <zhaojun@baixing.com>
2012-09-07 17:58:00 +0800

    depdents some trait files from service

---
+++ htdocs/lib/DataDelegateTrait.php
@@ -0,0 +1,27 @@
+<?
+trait DataDelegateTrait {
+	protected $data;
+
+	public function __construct($data = null) {
+		if ($data) $this->bind($data);
+	}
+	
+	public function bind($data) {
+		$this->data = $data;
+	}
+
+	public function __get($name) {
+		if (!$this->data) throw new Exception('Need bind data first!');
+		return $this->data->$name;
+	}
+
+	public function __set($name, $value) {
+		if (!$this->data) throw new Exception('Need bind data first!');
+		return $this->data->$name = $value;
+	}
+
+	public function __call($name, $args) {
+		if (!$this->data) throw new Exception('Need bind data first!');
+		return call_user_func_array(array($this->data, $name), $args);
+	}
+}
---
+++ htdocs/lib/ValidatorTrait.php
@@ -0,0 +1,25 @@
+<?
+
+trait ValidatorTrait {
+	//todo: implement errors and message.
+
+	protected function validate($exp, $value) {
+		if (is_array($exp) && !call_user_func_array(['self', $exp[0]], array_merge([$value], array_slice($exp, 1))))
+			return false;
+		elseif ($exp && is_null($this->$key))
+			return false;
+		return true;
+	}
+
+	protected static function validInt($value, $min = null, $max = null) {
+		return is_numeric($value) && (is_null($min) ? true : $value >= $min) && (is_null($max) ? true : $value <= $max);
+	}
+
+	protected static function validMobile($value) {
+		return preg_match('/^1(3|4|5|8)\d{9}$/', $value);
+	}
+
+	protected static function validStringLength($value, $min = null, $max = null) {
+		return self::validInt(mb_strlen($value, 'utf8'), $min, $max);
+	}
+}
