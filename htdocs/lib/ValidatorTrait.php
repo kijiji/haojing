84ffde5c4ef2b36859cf27fedab662b92fd5ab66
milkliker <zhaojun@baixing.com>
2012-09-10 17:00:57 +0800

    add comment and change validInt name to validNumber

--- htdocs/lib/ValidatorTrait.php
+++ htdocs/lib/ValidatorTrait.php
@@ -4,15 +4,23 @@
 trait ValidatorTrait {
 	//todo: implement errors and message.
 
+	/*
+	 * $exp can be bool and array:
+	 *   BOOL (0:none-required/1:required)
+	 *   Array ('function name', ['params']), eg:
+	 *      ['validNumber', 10, 100] should be valid for number between 10 and 100
+	 *      ['validMobile'] should be a mobile number
+	 */
 	protected function validate($exp, $value) {
-		if (is_array($exp) && !call_user_func_array(['self', $exp[0]], array_merge([$value], array_slice($exp, 1))))
+		if (is_array($exp) && !call_user_func_array(['self', $exp[0]], array_merge([$value], array_slice($exp, 1)))) {
 			return false;
-		elseif ($exp && is_null($this->$key))
+		} elseif ($exp && is_null($this->$key)) {
 			return false;
+		}
 		return true;
 	}
 
-	protected static function validInt($value, $min = null, $max = null) {
+	protected static function validNumber($value, $min = null, $max = null) {
 		return is_numeric($value) && (is_null($min) ? true : $value >= $min) && (is_null($max) ? true : $value <= $max);
 	}
 
@@ -21,6 +29,6 @@ trait ValidatorTrait {
 	}
 
 	protected static function validStringLength($value, $min = null, $max = null) {
-		return self::validInt(mb_strlen($value, 'utf8'), $min, $max);
+		return self::validNumber(mb_strlen($value, 'utf8'), $min, $max);
 	}
 }
