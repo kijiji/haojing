8a6057d8061cc103b42ca94f2ac84511b736d5f5
YuBing <icedfish@gmail.com>
2012-09-16 17:19:16 +0800

    cli换成用class实现
    用function可能导致重名。

--- cli.php
+++ cli.php
@@ -1,43 +1,49 @@
 #!/usr/bin/env php
 <?php
 //yubing@baixing.com
-//recommend setup: chmod +x /path/cli.php && ln -s /path/cli.php /usr/bin/haojing
+// recommend setup cmd : 
+// file='/home/haojing/cli.php' && chmod +x $file && ln -s $file /usr/bin/haojing
 
 include(__DIR__ . '/htdocs/init.php');
 
-function usage() {
-	echo "# Run php with haojing libs #\n";
-	echo "Usage: \n";
-	echo "\thaojing -r <php_code> \t run a piece of code \n";
-	echo "\thaojing [a.php] \t run a single php\n";
-	echo "\thaojing ['a.php?foo=bar&ping=pong'] \t \$_GET['foo'] is ready for use !\n";
-	echo "\t\t\t\t\t don't forget the '' for escape when using multi args\n";
-	exit(1);
-}
-
-function runCode($code) {
-	return eval($code);
-}
-
-function runFile($file) {
-	$pos = strpos($file, '?');
-	if ( $pos !== false ){
-		parse_str(substr($file, $pos + 1), $_GET);
-		$file = substr($file, 0, $pos);
+class Cli {
+	private function usage() {
+		echo "# Run php with haojing libs #\n";
+		echo "Usage: \n";
+		echo "\thaojing -r <php_code> \t run a piece of code \n";
+		echo "\thaojing [a.php] \t run a single php\n";
+		echo "\thaojing ['a.php?foo=bar&ping=pong'] \t \$_GET['foo'] is ready for use !\n";
+		echo "\t\t\t\t\t don't forget the '' for escape when using multi args\n";
+		exit(1);
 	}
-	include $file;
-}
 
-if ( count($argv) < 2 ) {
-	usage();
-}
-
-if ($argv[1] == '-r'){
-	if (isset($argv[2])) {
-		runCode($argv[2]);
-	} else {
-		usage();
+	private function runCode($code) {
+		return eval($code);
 	}
-} else {
-	runFile($argv[1]);
+
+	private function runFile($file) {
+		$pos = strpos($file, '?');
+		if ($pos !== false) {
+			parse_str(substr($file, $pos + 1), $_GET);
+			$file = substr($file, 0, $pos);
+		}
+		include $file;
+	}
+
+	public function __construct($argv) {
+		if (count($argv) < 2) {
+			$this->usage();
+		}
+		if ($argv[1] == '-r') {
+			if (isset($argv[2])) {
+				$this->runCode($argv[2]);
+			} else {
+				$this->usage();
+			}
+		} else {
+			$this->runFile($argv[1]);
+		}
+	}
 }
+
+new Cli($argv);
